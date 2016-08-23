28517ed9b4ed911a2a00feb95f85032e4d9f381a
Shwetha <shwetha.ravi3@gmail.com>
2016-08-23 23:04:51 +0800

    New message and a prediction.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,17 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		String message = "Thank God it's Tuesday";
+		System.out.println(message);
+		countLetters(message);
+	}
+
+	private void countLetters(String message) {
+		int letters = message.length;
+		weirdPrediction(letters);
+	}
+
+	private void weirdPrediction(int days) {
+		System.out.println("Your birthday is in " + days + " days.");
 	}
 }
